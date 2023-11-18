b6a32c6cbf7cd3381ffdb609176fa8107d58d768
NgocTrung <ngoctrungg02@gmail.com>
2023-11-18 15:06:16 +0700

    Add new project

---
+++ AdvanceEshop/Controllers/CartController.cs
@@ -0,0 +1,62 @@
+﻿using AdvanceEshop.Data;
+using AdvanceEshop.Infrastructure;
+using AdvanceEshop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvanceEshop.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CartController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Cart", HttpContext.Session.GetJson<Cart>("cart"));
+        }
+
+        public Cart? Cart { get; set; }
+        public IActionResult AddToCart(int productId)
+        {
+            Product? product = _context.Products
+                .FirstOrDefault(p => p.ProductId == productId);
+            if (product != null)
+            {
+                Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+                Cart.AddItem(product, 1);
+                HttpContext.Session.SetJson("cart", Cart);
+            }
+            return View("Cart", Cart);
+        }
+
+        public IActionResult UpdateCart(int productId)
+        {
+            Product? product = _context.Products
+                .FirstOrDefault(p => p.ProductId == productId);
+            if (product != null)
+            {
+                Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+                Cart.AddItem(product, -1);
+                HttpContext.Session.SetJson("cart", Cart);
+            }
+            return View("Cart", Cart);
+        }
+
+        public IActionResult RemoveFromCart(int productId)
+        {
+            Product? product = _context.Products
+                .FirstOrDefault(p => p.ProductId == productId);
+            if (product != null)
+            {
+                Cart = HttpContext.Session.GetJson<Cart>("cart");
+                Cart.RemoveLine(product);
+                HttpContext.Session.SetJson("cart", Cart);
+            }
+            return View("Cart", Cart);
+        }
+    }
+}
---
+++ AdvanceEshop/Controllers/PaymentController.cs
@@ -0,0 +1,84 @@
+﻿using AdvanceEshop.Data;
+using AdvanceEshop.Infrastructure;
+using AdvanceEshop.Models;
+using Microsoft.AspNetCore.Mvc;
+using static AdvanceEshop.Models.PaymentClient;
+
+namespace AdvanceEshop.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly PaypalClient _paypalClient;
+        private readonly ApplicationDbContext _context;
+
+        public PaymentController(PaypalClient paypalClient, ApplicationDbContext context)
+        {
+            _paypalClient = paypalClient;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return RedirectToAction();
+        }
+
+
+        public IActionResult PaypalDemo()
+        {
+            ViewBag.PaypalClientId = _paypalClient.ClientId;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PaypalOrder(CancellationToken cancellationToken)
+        {
+            // Tạo đơn hàng (thông tin lấy từ Session???)
+            Cart cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            var tongTien = cart.ComputeTotalValues().ToString("F2");
+            var donViTienTe = "USD";
+            // OrderId - mã tham chiếu (duy nhất)
+            var orderIdref = "DH" + DateTime.Now.Ticks.ToString();
+
+            try
+            {
+                // a. Create paypal order
+                var response = await _paypalClient.CreateOrder(tongTien, donViTienTe, orderIdref);
+
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                var error = new
+                {
+                    e.GetBaseException().Message
+                };
+
+                return BadRequest(error);
+            }
+        }
+
+        public async Task<IActionResult> PaypalCapture(string orderId, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var response = await _paypalClient.CaptureOrder(orderId);
+
+                var reference = response.purchase_units[0].reference_id;
+
+                // Put your logic to save the transaction here
+                // You can use the "reference" variable as a transaction key
+                // Lưu đơn hàng vô database
+
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                var error = new
+                {
+                    e.GetBaseException().Message
+                };
+
+                return BadRequest(error);
+            }
+        }
+    }
+}
