2fb8a855d9b4370d1f08d16e5422af7b3039d80d
Minh Nhut <phamminhnhut859@gmail.com>
2023-12-10 18:37:35 +0700

    SEO, Live Chat, Email Marketing

---
+++ AdvanceEshop/Controllers/CheckoutController.cs
@@ -0,0 +1,58 @@
+﻿using AdvanceEshop.Data;
+using AdvanceEshop.Infrastructure;
+using AdvanceEshop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Security.Claims;
+using Newtonsoft.Json;
+
+namespace AdvanceEshop.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<CheckoutController> _logger;
+
+        public CheckoutController(ApplicationDbContext context, ILogger<CheckoutController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        public async Task<IActionResult> Checkout()
+        {
+            var userEmail = User.FindFirstValue(ClaimTypes.Email);
+            if (userEmail == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                var ordercode = Guid.NewGuid().ToString();
+                var orderItem = new OrderModel();
+                orderItem.OrderCode = ordercode;
+                orderItem.UserName = userEmail;
+                orderItem.Status = 1;
+                orderItem.CreatedDate = DateTime.Now;
+                _context.Add(orderItem);
+                _context.SaveChanges();
+
+                List<CartItemModel> cartItems = HttpContext.Session.GetJson<List<CartItemModel>>("Cart") ?? new List<CartItemModel>();
+                foreach (var cart in cartItems)
+                {
+                    var orderdetails = new OrderDetails();
+                    orderdetails.UserName = userEmail;
+                    orderdetails.OrderCode = ordercode;
+                    orderdetails.ProductId = cart.ProductId;
+                    orderdetails.Price = cart.Price;
+                    orderdetails.Quantity = cart.Quantity;
+                    _context.Add(orderdetails);
+                    _context.SaveChanges();
+                }
+                HttpContext.Session.Remove("Cart");
+                TempData["success"] = " Checkout thành công, vui lòng chờ duyệt đơn hàng";
+                return RedirectToAction("Index", "Cart");
+            }
+        }
+
+    }
+}
